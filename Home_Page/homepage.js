6e104d0cc85162a4a22f4e588716c6606fd66a29
DevanNP34 <devannajwanprayogo@gmail.com>
2025-10-08 14:48:28 +0700

    add html & css of car pages

---
+++ Home_Page/homepage.js
@@ -0,0 +1,11 @@
+document.querySelector('.explore-btn').addEventListener('click', function() {
+  window.location.href = '../Explore_Page/explore.html';
+});
+
+document.querySelector('.try-btn').addEventListener('click', function() {
+  window.location.href = '../Explore_Page/explore.html';
+});
+
+document.querySelector('.book-btn').addEventListener('click', function() {
+  window.location.href = '../Testdrive_Page/testdrive.html';
+}); 
